average fix

primerEntregable/PrimerPrograma.py
@@ -9,10 +9,23 @@ samps = data.shape[0]
 n_channels = data.shape[1]
 win_size = 256
 
-print('Número de muestras: ', data.shape[0])
-print('Número de canales: ', data.shape[1])
-print('Duración del registro: ', samps / samp_rate, 'segundos')
-print(data)
+channPow1 = {
+
+}
+channPow2 = {
+
+}
+channPowAverage1 = {
+
+}
+channPowAverage2 = {
+    
+}
+
+# print('Número de muestras: ', data.shape[0])
+# print('Número de canales: ', data.shape[1])
+# print('Duración del registro: ', samps / samp_rate, 'segundos')
+# print(data)
 
 # Time channel
 time = data[:, 0]
@@ -23,6 +36,7 @@ chann2 = data[:, 3]
 
 # Mark data
 mark = data[:, 6]
+
 
 training_samples = {}
 for i in range(0, samps):
@@ -35,9 +49,14 @@ for i in range(0, samps):
         elif mark[i] == 200:
             if not condition_id in training_samples.keys():
                 training_samples[condition_id] = []
+                channPow1[condition_id] = {}
+                channPow2[condition_id] = {}
+                channPowAverage1[condition_id] = {}
+                channPowAverage2[condition_id] = {}
             training_samples[int(condition_id)].append([iniSamp, i])
 
-print('Rango de muestras con datos de entrenamiento:', training_samples)
+# print('Rango de muestras con datos de entrenamiento:', training_samples)
+print(channPowAverage1)
 
 # Plot data
 for currentMark in training_samples:
@@ -49,8 +68,9 @@ for currentMark in training_samples:
     t = time[ini_samp: end_samp]
     y = chann2[ini_samp: end_samp]
 
-    plt.plot(t, x, label='Canal 1')
-    plt.plot(t, y, color='red', label='Canal 2')
+    fig, axs = plt.subplots(2)
+    axs[0].plot(t, x, label='Canal 1')
+    axs[1].plot(t, y, color='red', label='Canal 2')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('micro V')
     plt.legend()
@@ -72,8 +92,7 @@ for currentMark in training_samples:
     start_index = np.where(freq >= 4.0)[0][0]
     end_index = np.where(freq >= 60.0)[0][0]
 
-    #print("Start index ", start_index)
-    #print("End index ", end_index)
+
 
     plt.plot(freq[start_index:end_index],
              power[start_index:end_index], label='Canal 1')
@@ -85,9 +104,20 @@ for currentMark in training_samples:
     plt.show()
 
 
+
+for mark in training_samples:
+    for hz in range(4, 61):
+            channPow1[mark][hz] = []
+            channPow2[mark][hz] = []
+            channPowAverage1[mark][hz] = 0
+            channPowAverage2[mark][hz] = 0
+
+
+
+print(channPow1, channPow2)
+
 for currentMark in training_samples:
-    espectroSum = []
-    espectroSum2 = []
+    
     for i in range(0, len(training_samples[currentMark])):
         ini_samp = training_samples[currentMark][i][0]
         end_samp = 0
@@ -102,7 +132,6 @@ for currentMark in training_samples:
             y = chann2[ini_samp: end_samp]
 
             print("You are currently at ", currentMark,"initial samp ", ini_samp, "end_samp ", end_samp)
-
             plt.plot(t, x, label='Canal 1')
             plt.plot(t, y, color='red', label='Canal 2')
             plt.xlabel('Tiempo (s)')
@@ -124,6 +153,10 @@ for currentMark in training_samples:
             start_index = np.where(freq >= 4.0)[0][0]
             end_index = np.where(freq >= 60.0)[0][0]
 
+            for hz in range(start_index, end_index+1):
+                channPow1[currentMark][hz].append(power[hz])
+                channPow2[currentMark][hz].append(power2[hz])
+
             plt.plot(freq[start_index:end_index], power[start_index:end_index], label='Canal 1')
             plt.plot(freq[start_index:end_index], power2[start_index:end_index], color='red', label='Canal 2')
             plt.xlabel('Hz')
@@ -131,7 +164,33 @@ for currentMark in training_samples:
             plt.legend()
             plt.clf()
             ini_samp = end_samp
-            espectroSum.extend(power[start_index:end_index])
-            espectroSum2.extend(power2[start_index:end_index])
-    print("Average Channel 1", sum(espectroSum)/len(espectroSum))
-    print("Average Channel 2", sum(espectroSum2)/len(espectroSum2))
+
+
+
+
+
+for mark in training_samples:
+    for hz in range(4, 61):
+        channPowAverage1[mark][hz] = sum(channPow1[mark][hz])/len(channPow1[mark][hz])
+        channPowAverage2[mark][hz] = sum(channPow1[mark][hz])/len(channPow1[mark][hz])
+
+
+print(channPowAverage1)
+print(channPowAverage2)
+
+
+# for mark in channPowAverage1:
+#     for hz in range(4, 61):
+#         fig, axs = plt.subplots(2)
+#         axs[0].plot(channPowAverage2[mark][hz])
+#         axs[1].plot()
+#          = sum(channPow2[mark][hz])/len(channPow2[mark][hz])
+
+    
+# print(channPowAverage1)
+# plt.plot(channPowAverage1.keys(), channPowAverage1.values())
+# plt.xlabel('Hz')
+# plt.ylabel('Power')
+# plt.legend()
+# plt.show()
+# print(channPowAverage1.keys())

primerEntregable/data_acquisition.py
@@ -11,6 +11,7 @@ n_channels = 5
 samp_rate = 256
 emg_data = [[] for i in range(n_channels)]
 samp_count = 0
+time_samp = []
 
 # Socket configuration
 UDP_IP = '127.0.0.1'
@@ -29,7 +30,7 @@ while True:
         values = np.frombuffer(data)       
         ns = int(len(values)/n_channels)
         samp_count+=ns        
-
+        time_samp.append(samp_count/samp_rate)
         for i in range(ns):
             for j in range(n_channels):
                 emg_data[j].append(values[n_channels*i + j])
@@ -45,24 +46,25 @@ while True:
             print("EMG_DATA ", ventana_actual)
             chann1 = ventana_actual[:, 0]
             chann2 = ventana_actual[:, 2]
-            print("Channel 1: ", chann1)
-            print("Channel 2: ", chann2)
 
-            ini_samp = start_time
-            end_samp = elapsed_time
+            # print("Channel 1: ", chann1)
+            # print("Channel 2: ", chann2)
+
+            ini_samp = samp_count-samp_rate
+            end_samp = samp_count
             
 
             x = chann1[ini_samp: end_samp]
-            t = [ini_samp, end_samp]
+            t = time_samp[ini_samp: end_samp]
             y = chann2[ini_samp: end_samp]
             
-            plt.plot(t, x, label='Canal 1')
-            plt.plot(t, y, color='red', label='Canal 2')
+            fig, axs = plt.subplots(2)
+            axs[0].plot(t, x, label='Canal 1')
+            axs[1].plot(t, y, color='red', label='Canal 2')
             plt.xlabel('Tiempo (s)')
             plt.ylabel('micro V')
             plt.legend()
             plt.show()
-
             
 
             # power, freq = plt.psd(x, NFFT=win_size, Fs=samp_rate)